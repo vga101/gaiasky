83bd524f14b37121b1d793bc9189f0ffcfcd153b
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2016-04-13 13:39:22 +0200

    The destkop:dist target produces results which work again

--- desktop/exe/gaiasky.bat
+++ desktop/exe/gaiasky.bat
@@ -2,4 +2,4 @@
 :: Gaia Sandbox start script.
 ::
 
-java -Xmx512m -XX:+UseParallelGC -cp "lib/*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSandboxDesktop
+java -Xmx512m -XX:+UseParallelGC -cp "lib/*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
