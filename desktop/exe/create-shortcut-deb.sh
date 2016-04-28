b3f42164de4a59d9ec7bb310f86bbe49dd10decc
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2016-04-28 12:27:59 +0200

    Some scripts updated

--- desktop/exe/create-shortcut-deb.sh
+++ desktop/exe/create-shortcut-deb.sh
@@ -1,2 +1,2 @@
 # Copy shortcut to applications folder
-cp $I4J_INSTALL_LOCATION/gaiasky.desktop /usr/share/applications/gaiasky.desktop
+cp $I4J_INSTALL_LOCATION/gs.desktop /usr/share/applications/gaiasky.desktop
