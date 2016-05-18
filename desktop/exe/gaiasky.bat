cd6239ec624bf9919fcc304738e992e866795cc9
Jumping Langur <toninoni@gmail.com>
2016-05-18 09:50:35 +0200

    Updated GC and memory settings for TGAS

--- desktop/exe/gaiasky.bat
+++ desktop/exe/gaiasky.bat
@@ -2,4 +2,4 @@
 :: Gaia Sandbox start script.
 ::
 
-java -Xmx512m -XX:+UseParallelGC -cp "lib/*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
+java -Xms1g -Xmx2g -XX:+UseConcMarkSweepGC -cp "lib/*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
