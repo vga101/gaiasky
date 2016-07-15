4231eb8ccb4082d0858292fb534dfd47709b3308
Jumping Langur <toninoni@gmail.com>
2016-07-15 11:55:48 +0200

    Fixed star point shader with smaller light point

--- android/assets/shader/point.fragment.glsl
+++ android/assets/shader/point.fragment.glsl
@@ -12,5 +12,5 @@ void main() {
 	vec2 uv = vec2(gl_PointCoord.s, gl_PointCoord.t);
 	uv.y = uv.y / u_ar;
 	float dist = distance(vec2(0.5), uv) * 2.0;
-    gl_FragColor = vec4(v_col.rgb, v_col.a * pow(1.0 - dist, 4.0));
+    gl_FragColor = vec4(v_col.rgb, v_col.a * pow(1.0 - dist, 6.0));
 }
