06b570db2ade15a5eb3f0def351253090b271684
Jumping Langur <toninoni@gmail.com>
2016-08-15 15:14:09 +0200

    Adjusted star point size and fixed anaglyphic 3D (red left, cyan right)

--- core/src/gaia/cu9/ari/gaiaorbit/render/system/PixelRenderSystem.java
+++ core/src/gaia/cu9/ari/gaiaorbit/render/system/PixelRenderSystem.java
@@ -37,7 +37,7 @@ public class PixelRenderSystem extends ImmediateRenderSystem implements IObserve
     public PixelRenderSystem(RenderGroup rg, int priority, float[] alphas) {
         super(rg, priority, alphas);
         EventManager.instance.subscribe(this, Events.TRANSIT_COLOUR_CMD, Events.ONLY_OBSERVED_STARS_CMD);
-        BRIGHTNESS_FACTOR = Constants.webgl ? 15f : 4f;
+        BRIGHTNESS_FACTOR = Constants.webgl ? 15f : 10f;
         updatePointSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
 
     }
