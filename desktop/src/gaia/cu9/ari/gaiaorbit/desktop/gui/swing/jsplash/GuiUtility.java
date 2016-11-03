905da091679baa7214e12b3ddaf20d80ecf93204
Toni Sagrista <tsagrista@ari.uni-heidelberg.de>
2016-11-03 12:14:02 +0100

    Fixed config dialog positioning
    This fixes #90

--- desktop/src/gaia/cu9/ari/gaiaorbit/desktop/gui/swing/jsplash/GuiUtility.java
+++ desktop/src/gaia/cu9/ari/gaiaorbit/desktop/gui/swing/jsplash/GuiUtility.java
@@ -1,19 +1,26 @@
 package gaia.cu9.ari.gaiaorbit.desktop.gui.swing.jsplash;
 
 import java.awt.Dimension;
-import java.awt.Toolkit;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.MouseInfo;
+import java.awt.Rectangle;
 import java.awt.Window;
 
-public class GuiUtility
-{
+public class GuiUtility {
     public GuiUtility() {
     }
 
-    public static void centerOnScreen(Window w)
-    {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    /**
+     * Centers the given window on the screen where the mouse is
+     * @param w
+     */
+    public static void centerOnScreen(Window w) {
+        // Get graphics device where the mouse is
+        GraphicsDevice currentMouseScreen = MouseInfo.getPointerInfo().getDevice();
+        GraphicsConfiguration gc = currentMouseScreen.getDefaultConfiguration();
+        Rectangle screenSize = gc.getBounds();
         Dimension windowSize = w.getPreferredSize();
-        w.setLocation(screenSize.width / 2 - windowSize.width / 2,
-                screenSize.height / 2 - windowSize.height / 2);
+        w.setLocation(screenSize.x + screenSize.width / 2 - windowSize.width / 2, screenSize.y + screenSize.height / 2 - windowSize.height / 2);
     }
 }
