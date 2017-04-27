5a395268820f53738f46f1a27380e8dcbf22ce24
langurmonkey <toninoni@gmail.com>
2017-04-27 08:39:20 +0200

    Added more comprehensive main to analytics module

--- desktop/src/gaia/cu9/ari/gaiaorbit/analytics/AnalyticsReporting.java
+++ desktop/src/gaia/cu9/ari/gaiaorbit/analytics/AnalyticsReporting.java
@@ -1,6 +1,10 @@
 package gaia.cu9.ari.gaiaorbit.analytics;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+
+import org.apache.http.NameValuePair;
 
 import com.brsanthu.googleanalytics.GoogleAnalytics;
 import com.brsanthu.googleanalytics.GoogleAnalyticsResponse;
@@ -14,11 +18,25 @@ public class AnalyticsReporting {
     }
 
     public static void main(String[] args) {
-        report();
+        Future<GoogleAnalyticsResponse> f = report();
         try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            GoogleAnalyticsResponse gar = f.get();
+
+            System.out.println("Response status code: " + gar.getStatusCode());
+            List<NameValuePair> result = gar.getPostedParms();
+            if (result != null)
+                for (NameValuePair pair : result) {
+                    System.out.println(pair.toString());
+                }
+            else
+                System.out.println("No params");
+        } catch (InterruptedException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (ExecutionException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
         }
+
     }
 }
