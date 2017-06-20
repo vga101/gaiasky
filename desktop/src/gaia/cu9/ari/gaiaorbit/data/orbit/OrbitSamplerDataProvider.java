9cf7596eda6111e2b3d2c838dfc10079a24d1d27
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2017-06-20 13:24:28 +0200

    Orbits resampled, moved to own folder
    - Orbits have been resampled
    - Orbits reside now in own folder

--- desktop/src/gaia/cu9/ari/gaiaorbit/data/orbit/OrbitSamplerDataProvider.java
+++ desktop/src/gaia/cu9/ari/gaiaorbit/data/orbit/OrbitSamplerDataProvider.java
@@ -14,12 +14,14 @@ import gaia.cu9.ari.gaiaorbit.data.orbit.OrbitDataLoader.OrbitDataLoaderParamete
 import gaia.cu9.ari.gaiaorbit.desktop.format.DesktopDateFormatFactory;
 import gaia.cu9.ari.gaiaorbit.desktop.format.DesktopNumberFormatFactory;
 import gaia.cu9.ari.gaiaorbit.desktop.util.DesktopConfInit;
+import gaia.cu9.ari.gaiaorbit.desktop.util.DesktopSysUtilsFactory;
 import gaia.cu9.ari.gaiaorbit.event.EventManager;
 import gaia.cu9.ari.gaiaorbit.event.Events;
 import gaia.cu9.ari.gaiaorbit.event.IObserver;
 import gaia.cu9.ari.gaiaorbit.util.ConfInit;
 import gaia.cu9.ari.gaiaorbit.util.I18n;
 import gaia.cu9.ari.gaiaorbit.util.Logger;
+import gaia.cu9.ari.gaiaorbit.util.SysUtilsFactory;
 import gaia.cu9.ari.gaiaorbit.util.concurrent.SingleThreadLocalFactory;
 import gaia.cu9.ari.gaiaorbit.util.concurrent.ThreadLocalFactory;
 import gaia.cu9.ari.gaiaorbit.util.coord.AstroUtils;
@@ -40,128 +42,124 @@ public class OrbitSamplerDataProvider implements IOrbitDataProvider, IObserver {
     OrbitData data;
 
     public static void main(String[] args) {
-	try {
-	    // Assets location
-	    String ASSETS_LOC = (System.getProperty("assets.location") != null ? System.getProperty("assets.location")
-		    : "");
+        try {
+            // Assets location
+            String ASSETS_LOC = (System.getProperty("assets.location") != null ? System.getProperty("assets.location") : "");
 
-	    Gdx.files = new Lwjgl3Files();
+            Gdx.files = new Lwjgl3Files();
 
-	    // Initialize number format
-	    NumberFormatFactory.initialize(new DesktopNumberFormatFactory());
+            // Sys utils
+            SysUtilsFactory.initialize(new DesktopSysUtilsFactory());
 
-	    // Initialize date format
-	    DateFormatFactory.initialize(new DesktopDateFormatFactory());
+            // Initialize number format
+            NumberFormatFactory.initialize(new DesktopNumberFormatFactory());
 
-	    ConfInit.initialize(
-		    new DesktopConfInit(new FileInputStream(new File(ASSETS_LOC + "conf/global.properties")),
-			    new FileInputStream(new File(ASSETS_LOC + "data/dummyversion"))));
+            // Initialize date format
+            DateFormatFactory.initialize(new DesktopDateFormatFactory());
 
-	    I18n.initialize(new FileHandle(ASSETS_LOC + "i18n/gsbundle"));
+            ConfInit.initialize(new DesktopConfInit(new FileInputStream(new File(ASSETS_LOC + "conf/global.properties")), new FileInputStream(new File(ASSETS_LOC + "data/dummyversion"))));
 
-	    ThreadLocalFactory.initialize(new SingleThreadLocalFactory());
+            I18n.initialize(new FileHandle(ASSETS_LOC + "i18n/gsbundle"));
 
-	    OrbitSamplerDataProvider.writeData = true;
-	    OrbitSamplerDataProvider me = new OrbitSamplerDataProvider();
-	    EventManager.instance.subscribe(me, Events.JAVA_EXCEPTION, Events.POST_NOTIFICATION);
+            ThreadLocalFactory.initialize(new SingleThreadLocalFactory());
 
-	    Date now = new Date();
-	    String[] bodies = new String[] { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus",
-		    "Neptune", "Moon", "Pluto" };
-	    float[] periods = new float[] { 87.9691f, 224.701f, 365.256363f, 686.971f, 4332.59f, 10759.22f, 30799.095f,
-		    60190.03f, 27.321682f, 90560f };
-	    for (int i = 0; i < bodies.length; i++) {
+            OrbitSamplerDataProvider.writeData = true;
+            OrbitSamplerDataProvider me = new OrbitSamplerDataProvider();
+            EventManager.instance.subscribe(me, Events.JAVA_EXCEPTION, Events.POST_NOTIFICATION);
 
-		String b = bodies[i];
-		float period = periods[i];
-		OrbitDataLoaderParameter param = new OrbitDataLoaderParameter(me.getClass(), b, now, true, period, 80);
-		me.load(null, param);
+            Date now = new Date();
+            String[] bodies = new String[] { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Moon", "Pluto" };
+            float[] periods = new float[] { 87.9691f, 224.701f, 365.256363f, 686.971f, 4332.59f, 10759.22f, 30799.095f, 60190.03f, 27.321682f, 90560f };
+            for (int i = 0; i < bodies.length; i++) {
 
-	    }
+                String b = bodies[i];
+                float period = periods[i];
+                OrbitDataLoaderParameter param = new OrbitDataLoaderParameter(me.getClass(), b, now, true, period, 80);
+                me.load(null, param);
 
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void load(String file, OrbitDataLoaderParameter parameter) {
-	// Sample using VSOP
-	int numSamples = parameter.numSamples > 0 ? parameter.numSamples : (int) (200 * parameter.orbitalPeriod / 365);
-	numSamples = Math.max(50, Math.min(1000, numSamples));
-	data = new OrbitData();
-	String bodyDesc = parameter.name;
-	Date d = new Date(parameter.ini.getTime());
-	double last = 0, accum = 0;
-	Vector3d ecl = new Vector3d();
+        // Sample using VSOP
+        int numSamples = parameter.numSamples > 0 ? parameter.numSamples : (int) (200 * parameter.orbitalPeriod / 365);
+        numSamples = Math.max(50, Math.min(1000, numSamples));
+        data = new OrbitData();
+        String bodyDesc = parameter.name;
+        Date d = new Date(parameter.ini.getTime());
+        double last = 0, accum = 0;
+        Vector3d ecl = new Vector3d();
 
-	// Milliseconds of this orbit in one revolution
-	long orbitalMs = (long) parameter.orbitalPeriod * 24 * 60 * 60 * 1000;
-	long stepMs = orbitalMs / numSamples;
+        // Milliseconds of this orbit in one revolution
+        long orbitalMs = (long) parameter.orbitalPeriod * 24 * 60 * 60 * 1000;
+        long stepMs = orbitalMs / numSamples;
 
-	// Load vsop orbit data
-	for (int i = 0; i <= numSamples; i++) {
-	    AstroUtils.getEclipticCoordinates(bodyDesc, d, ecl);
+        // Load vsop orbit data
+        for (int i = 0; i <= numSamples; i++) {
+            AstroUtils.getEclipticCoordinates(bodyDesc, d, ecl);
 
-	    if (last == 0) {
-		last = Math.toDegrees(ecl.x);
-	    }
+            if (last == 0) {
+                last = Math.toDegrees(ecl.x);
+            }
 
-	    accum += Math.toDegrees(ecl.x) - last;
-	    last = Math.toDegrees(ecl.x);
+            accum += Math.toDegrees(ecl.x) - last;
+            last = Math.toDegrees(ecl.x);
 
-	    if (accum > 355) {
-		break;
-	    }
+            if (accum > 355) {
+                break;
+            }
 
-	    Coordinates.sphericalToCartesian(ecl, ecl);
-	    ecl.mul(Coordinates.equatorialToEcliptic()).scl(1);
-	    data.x.add(ecl.x);
-	    data.y.add(ecl.y);
-	    data.z.add(ecl.z);
-	    d.setTime(d.getTime() + stepMs);
-	    data.time.add(new Date(d.getTime()));
-	}
+            Coordinates.sphericalToCartesian(ecl, ecl);
+            ecl.mul(Coordinates.equatorialToEcliptic()).scl(1);
+            data.x.add(ecl.x);
+            data.y.add(ecl.y);
+            data.z.add(ecl.z);
+            d.setTime(d.getTime() + stepMs);
+            data.time.add(new Date(d.getTime()));
+        }
 
-	// Close the circle
-	data.x.add(data.x.get(0));
-	data.y.add(data.y.get(0));
-	data.z.add(data.z.get(0));
-	d.setTime(d.getTime() + stepMs);
-	data.time.add(new Date(d.getTime()));
+        // Close the circle
+        data.x.add(data.x.get(0));
+        data.y.add(data.y.get(0));
+        data.z.add(data.z.get(0));
+        d.setTime(d.getTime() + stepMs);
+        data.time.add(new Date(d.getTime()));
 
-	if (writeData) {
-	    try {
-		OrbitDataWriter.writeOrbitData(writeDataPath + "orb." + bodyDesc.toString().toUpperCase() + ".dat",
-			data);
-	    } catch (IOException e) {
-		Logger.error(e);
-	    }
-	}
+        if (writeData) {
+            try {
+                OrbitDataWriter.writeOrbitData(writeDataPath + "orb." + bodyDesc.toString().toUpperCase() + ".dat", data);
+            } catch (IOException e) {
+                Logger.error(e);
+            }
+        }
 
-	Logger.info(this.getClass().getSimpleName(),
-		I18n.bundle.format("notif.orbitdataof.loaded", parameter.name, data.getNumPoints()));
+        Logger.info(this.getClass().getSimpleName(), I18n.bundle.format("notif.orbitdataof.loaded", parameter.name, data.getNumPoints()));
 
     }
 
     public OrbitData getData() {
-	return data;
+        return data;
     }
 
     @Override
     public void notify(Events event, Object... data) {
-	switch (event) {
-	case JAVA_EXCEPTION:
-	    System.err.println((Exception) data[0]);
-	    break;
-	case POST_NOTIFICATION:
-	    System.out.println((String) data[0] + " -" + (String) data[1]);
-	}
+        switch (event) {
+        case JAVA_EXCEPTION:
+            System.err.println((Exception) data[0]);
+            break;
+        case POST_NOTIFICATION:
+            System.out.println((String) data[0] + " -" + (String) data[1]);
+        }
 
     }
 
