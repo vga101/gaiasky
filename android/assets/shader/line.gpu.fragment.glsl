5f3e637cc06ece7f5ea6f0b4d6827f55d55f4238
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-01-09 11:59:11 +0100

    Discarded lines with alpha=0

--- android/assets/shader/line.gpu.fragment.glsl
+++ android/assets/shader/line.gpu.fragment.glsl
@@ -8,5 +8,8 @@ uniform float u_alpha;
 varying vec4 v_col;
 void main() {
     float alpha = v_col.a * u_alpha;
+    if(alpha == 0.0) {
+        discard;
+    }
     gl_FragColor = vec4(v_col.rgb + pow(alpha, 4.0) * 2.0, alpha);
 }
