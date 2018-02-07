8875e3f6053aef935e2680306fd7a7b12b2ddfad
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-02-07 16:26:46 +0100

    Removed skinning from shaders
    - Skinning does not apply in Gaia Sky and is not supported (no characters), so we stripped it off.

--- android/assets/shader/default.vertex.glsl
+++ android/assets/shader/default.vertex.glsl
@@ -29,65 +29,6 @@ attribute vec2 a_texCoord0;
 varying vec2 v_texCoords0;
 #endif // textureFlag
 
-#ifdef boneWeight0Flag
-#define boneWeightsFlag
-attribute vec2 a_boneWeight0;
-#endif //boneWeight0Flag
-
-#ifdef boneWeight1Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight1;
-#endif //boneWeight1Flag
-
-#ifdef boneWeight2Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight2;
-#endif //boneWeight2Flag
-
-#ifdef boneWeight3Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight3;
-#endif //boneWeight3Flag
-
-#ifdef boneWeight4Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight4;
-#endif //boneWeight4Flag
-
-#ifdef boneWeight5Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight5;
-#endif //boneWeight5Flag
-
-#ifdef boneWeight6Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight6;
-#endif //boneWeight6Flag
-
-#ifdef boneWeight7Flag
-#ifndef boneWeightsFlag
-#define boneWeightsFlag
-#endif
-attribute vec2 a_boneWeight7;
-#endif //boneWeight7Flag
-
-#if defined(numBones) && defined(boneWeightsFlag)
-#if (numBones > 0) 
-#define skinningFlag
-#endif
-#endif
 
 uniform mat4 u_worldTrans;
 
@@ -283,39 +224,8 @@ void main() {
 		#endif //alphaTestFlag
 	#endif // blendedFlag
 
-	#ifdef skinningFlag
-		mat4 skinning = mat4(0.0);
-		#ifdef boneWeight0Flag
-			skinning += (a_boneWeight0.y) * u_bones[int(a_boneWeight0.x)];
-		#endif //boneWeight0Flag
-		#ifdef boneWeight1Flag				
-			skinning += (a_boneWeight1.y) * u_bones[int(a_boneWeight1.x)];
-		#endif //boneWeight1Flag
-		#ifdef boneWeight2Flag		
-			skinning += (a_boneWeight2.y) * u_bones[int(a_boneWeight2.x)];
-		#endif //boneWeight2Flag
-		#ifdef boneWeight3Flag
-			skinning += (a_boneWeight3.y) * u_bones[int(a_boneWeight3.x)];
-		#endif //boneWeight3Flag
-		#ifdef boneWeight4Flag
-			skinning += (a_boneWeight4.y) * u_bones[int(a_boneWeight4.x)];
-		#endif //boneWeight4Flag
-		#ifdef boneWeight5Flag
-			skinning += (a_boneWeight5.y) * u_bones[int(a_boneWeight5.x)];
-		#endif //boneWeight5Flag
-		#ifdef boneWeight6Flag
-			skinning += (a_boneWeight6.y) * u_bones[int(a_boneWeight6.x)];
-		#endif //boneWeight6Flag
-		#ifdef boneWeight7Flag
-			skinning += (a_boneWeight7.y) * u_bones[int(a_boneWeight7.x)];
-		#endif //boneWeight7Flag
-	#endif //skinningFlag
 
-	#ifdef skinningFlag
-		vec4 pos = u_worldTrans * skinning * vec4(a_position, 1.0);
-	#else
-		vec4 pos = u_worldTrans * vec4(a_position, 1.0);
-	#endif
+	vec4 pos = u_worldTrans * vec4(a_position, 1.0);
 
 	gl_Position = u_projViewTrans * pos;
 
@@ -326,11 +236,7 @@ void main() {
 	#endif //shadowMapFlag
 
 	#if defined(normalFlag)
-		#if defined(skinningFlag)
-			vec3 normal = normalize((u_worldTrans * skinning * vec4(a_normal, 0.0)).xyz);
-		#else
-			vec3 normal = normalize(u_normalMatrix * a_normal);
-		#endif
+		vec3 normal = normalize(u_normalMatrix * a_normal);
 		v_normal = normal;
 	#endif // normalFlag
 
--- android/assets/shader/normal.vertex.glsl
+++ android/assets/shader/normal.vertex.glsl
@@ -273,111 +273,6 @@ varying float v_alphaTest;
     uniform sampler2D u_bumpTexture;
 #endif
 
-////////////////////////////////////////////////////////////////////////////////////
-////////// SKINNING
-///////////////////////////////////////////////////////////////////////////////////
-#ifdef boneWeight0Flag
-    #define boneWeightsFlag
-    attribute vec2 a_boneWeight0;
-#endif //boneWeight0Flag
-
-#ifdef boneWeight1Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight1;
-#endif //boneWeight1Flag
-
-#ifdef boneWeight2Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight2;
-#endif //boneWeight2Flag
-
-#ifdef boneWeight3Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight3;
-#endif //boneWeight3Flag
-
-#ifdef boneWeight4Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight4;
-#endif //boneWeight4Flag
-
-#ifdef boneWeight5Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight5;
-#endif //boneWeight5Flag
-
-#ifdef boneWeight6Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight6;
-#endif //boneWeight6Flag
-
-#ifdef boneWeight7Flag
-    #ifndef boneWeightsFlag
-	#define boneWeightsFlag
-    #endif
-    attribute vec2 a_boneWeight7;
-#endif //boneWeight7Flag
-
-#if defined(numBones) && defined(boneWeightsFlag)
-    #if (numBones > 0) 
-	#define skinningFlag
-    #endif
-#endif
-
-#if defined(numBones)
-    #if numBones > 0
-	uniform mat4 u_bones[numBones];
-    #endif //numBones
-#endif
-
-#ifdef skinningFlag
-    mat4 skinning = mat4(0.0);
-    #ifdef boneWeight0Flag
-	skinning += (a_boneWeight0.y) * u_bones[int(a_boneWeight0.x)];
-    #endif //boneWeight0Flag
-    #ifdef boneWeight1Flag				
-	skinning += (a_boneWeight1.y) * u_bones[int(a_boneWeight1.x)];
-    #endif //boneWeight1Flag
-    #ifdef boneWeight2Flag		
-	skinning += (a_boneWeight2.y) * u_bones[int(a_boneWeight2.x)];
-    #endif //boneWeight2Flag
-    #ifdef boneWeight3Flag
-	skinning += (a_boneWeight3.y) * u_bones[int(a_boneWeight3.x)];
-    #endif //boneWeight3Flag
-    #ifdef boneWeight4Flag
-	skinning += (a_boneWeight4.y) * u_bones[int(a_boneWeight4.x)];
-    #endif //boneWeight4Flag
-    #ifdef boneWeight5Flag
-	skinning += (a_boneWeight5.y) * u_bones[int(a_boneWeight5.x)];
-    #endif //boneWeight5Flag
-    #ifdef boneWeight6Flag
-	skinning += (a_boneWeight6.y) * u_bones[int(a_boneWeight6.x)];
-    #endif //boneWeight6Flag
-    #ifdef boneWeight7Flag
-	skinning += (a_boneWeight7.y) * u_bones[int(a_boneWeight7.x)];
-    #endif //boneWeight7Flag
-#endif //skinningFlag
-
-#ifdef skinningFlag
-    vec3 applySkinning(const in vec3 x)
-    {   return (skinning * vec4(x, 0.0)).xyz;}
-    vec4 applySkinning(const in vec4 x)
-    {   return (skinning * x);}
-#else
-    #define applySkinning(x) x
-#endif //skinningFlag
 
 #if defined(diffuseTextureFlag) || defined(specularTextureFlag)
     #define textureFlag
@@ -492,10 +387,9 @@ void main() {
 
     calculateTangentVectors();
 
-    g_position = applySkinning(g_position);
-    g_normal = normalize(u_normalMatrix * applySkinning(g_normal));
-    g_binormal = normalize(u_normalMatrix * applySkinning(g_binormal));
-    g_tangent = normalize(u_normalMatrix * applySkinning(g_tangent));
+    g_normal = normalize(u_normalMatrix * g_normal);
+    g_binormal = normalize(u_normalMatrix * g_binormal);
+    g_tangent = normalize(u_normalMatrix * g_tangent);
 
     g_position = u_worldTrans * g_position;
     gl_Position = u_projViewTrans * g_position;
