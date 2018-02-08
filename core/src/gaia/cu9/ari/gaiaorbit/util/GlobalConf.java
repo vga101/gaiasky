35c484aa9025ca0c46150d0803cab6e7f6d6026a
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-02-08 16:23:19 +0100

    Again, disabled relativistic effects by default

--- core/src/gaia/cu9/ari/gaiaorbit/util/GlobalConf.java
+++ core/src/gaia/cu9/ari/gaiaorbit/util/GlobalConf.java
@@ -195,7 +195,7 @@ public class GlobalConf {
         public boolean STRIPPED_FOV_MODE = false;
         /** Whether octree drawing is active or not **/
         public boolean DRAW_OCTREE;
-        public boolean RELATIVISTIC_ABERRATION = true;
+        public boolean RELATIVISTIC_ABERRATION = false;
 
         public RuntimeConf() {
             EventManager.instance.subscribe(this, Events.LIMIT_MAG_CMD, Events.INPUT_ENABLED_CMD, Events.DISPLAY_GUI_CMD, Events.TOGGLE_UPDATEPAUSE, Events.TOGGLE_TIME_CMD, Events.RECORD_CAMERA_CMD);
