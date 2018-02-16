9f0d8da6dce95e5c1da276fa085f2f41b6ee6719
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-02-16 12:13:50 +0100

    Fixed run scripts to run with memory-mapped files

--- core/exe/gaiasky.cmd
+++ core/exe/gaiasky.cmd
@@ -3,4 +3,4 @@
 ::
 
 
-.\jre\bin\java -Xms2g -Xmx3g -XX:+UseConcMarkSweepGC -cp "lib\*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
+.\jre\bin\java -Xms2g -Xmx3g -XX:+UseConcMarkSweepGC -Dassets.location=.\ -cp "lib\*" gaia.cu9.ari.gaiaorbit.desktop.GaiaSkyDesktop
