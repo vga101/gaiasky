0327597283858022878f2a0203948cf7d4c772c0
langurmonkey <toninoni@gmail.com>
2018-03-01 14:17:52 +0100

    Bloom threshold lowered

--- core/src/gaia/cu9/ari/gaiaorbit/desktop/render/DesktopPostProcessor.java
+++ core/src/gaia/cu9/ari/gaiaorbit/desktop/render/DesktopPostProcessor.java
@@ -179,7 +179,7 @@ public class DesktopPostProcessor implements IPostProcessor, IObserver {
         // BLOOM
         ppb.bloom = new Bloom((int) (width * bloomFboScale), (int) (height * bloomFboScale));
         ppb.bloom.setBloomIntesity(GlobalConf.postprocess.POSTPROCESS_BLOOM_INTENSITY);
-        ppb.bloom.setThreshold(0.8f);
+        ppb.bloom.setThreshold(0.3f);
         ppb.bloom.setEnabled(GlobalConf.postprocess.POSTPROCESS_BLOOM_INTENSITY > 0);
         ppb.pp.addEffect(ppb.bloom);
 
