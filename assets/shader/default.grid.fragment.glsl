5a6064b71378e0c5d6e9842123ec25adb641e96c
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-05-24 15:27:23 +0200

    Added circular mesh to shader

--- assets/shader/default.grid.fragment.glsl
+++ assets/shader/default.grid.fragment.glsl
@@ -13,8 +13,29 @@ varying float v_opacity;
 varying vec4 v_color;
 
 #define PI 3.141592
+#define N 10.0
 
-vec4 spherical(vec2 tc) {
+vec4 circle(vec2 tc) {
+    // in [-1..1]
+    tc = (tc - 0.5) * 2.0;
+    float alpha = 1.3 - length(tc);
+    
+    if(length(tc) > 1.01){
+        discard;
+    }
+    
+    vec2 coord = tc * (N - 1.0) * 2.0;
+    float dist = length(coord);
+    
+    // the grid in itself
+    float func = cos(PI*dist);
+
+    vec4 result = u_diffuseColor * smoothstep(0.999, 1.0, func);
+    result.a *= alpha;
+    return result;
+}
+
+vec4 square(vec2 tc) {
     vec2 coord = tc;
     coord.x *= 36.0 * 2.0;
     coord.y *= 18.0 * 2.0;
@@ -32,5 +53,5 @@ vec4 spherical(vec2 tc) {
 }
 
 void main() {
-    gl_FragColor = spherical(v_texCoords0);
+    gl_FragColor = square(v_texCoords0);
 }
