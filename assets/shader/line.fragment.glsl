0831f7edc0bfc97755f4d26097d5e8571662c139
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-06-22 08:33:29 +0200

    Line colors for GL lines fixed

--- assets/shader/line.fragment.glsl
+++ assets/shader/line.fragment.glsl
@@ -5,6 +5,5 @@ precision mediump int;
 
 varying vec4 v_col;
 void main() {
-    float alpha = v_col.a;
-    gl_FragColor = vec4(v_col.rgb + pow(alpha, 4.0) * 2.0, alpha);
+    gl_FragColor = v_col;
 }
