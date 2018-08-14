51093d6b4dcbf660c737958262229098f5108408
langurmonkey <tsagrista@ari.uni-heidelberg.de>
2018-08-14 15:00:47 +0200

    Analytics instance not created if reporting disabled

--- core/src/gaia/cu9/ari/gaiaorbit/analytics/AnalyticsReporting.java
+++ core/src/gaia/cu9/ari/gaiaorbit/analytics/AnalyticsReporting.java
@@ -23,102 +23,105 @@ import gaia.cu9.ari.gaiaorbit.util.format.DateFormatFactory;
 
 public class AnalyticsReporting {
 
-    private static long startTimeMs;
+	private static long startTimeMs;
 
-    private static AnalyticsReporting instance;
+	private static AnalyticsReporting instance;
 
-    public static void initialize(IPermission permission) {
-        if (instance == null)
-            instance = new AnalyticsReporting(permission);
-    }
+	public static void initialize(IPermission permission) {
+		if (instance == null)
+			instance = new AnalyticsReporting(permission);
+	}
 
-    public static AnalyticsReporting getInstance() {
-        initialize(null);
-        return instance;
-    }
+	public static AnalyticsReporting getInstance() {
+		initialize(null);
+		return instance;
+	}
 
-    private final GoogleAnalytics ga;
-    private final String appname, languagetag, version;
-    private IPermission permission;
+	private final GoogleAnalytics ga;
+	private final String appname, languagetag, version;
+	private IPermission permission;
 
-    private AnalyticsReporting(IPermission permission) {
-        this.ga = new GoogleAnalytics("UA-57580474-6");
-        this.appname = GlobalConf.APPLICATION_NAME;
-        this.languagetag = I18n.locale.toLanguageTag().toLowerCase();
-        this.version = GlobalConf.version.version + "." + GlobalConf.version.build;
-        this.permission = permission;
-    }
+	private AnalyticsReporting(IPermission permission) {
+		if (permission.check())
+			this.ga = new GoogleAnalytics("UA-57580474-6");
+		else
+			this.ga = null;
+		this.appname = GlobalConf.APPLICATION_NAME;
+		this.languagetag = I18n.locale.toLanguageTag().toLowerCase();
+		this.version = GlobalConf.version.version + "." + GlobalConf.version.build;
+		this.permission = permission;
+	}
 
-    public Future<GoogleAnalyticsResponse> sendStartAppReport() {
-        if (!permission.check())
-            return null;
-        PageViewHit pvh = new PageViewHit("http://gaiasky.com", appname + " start");
-        pvh.applicationName(appname);
-        pvh.applicationVersion(version);
-        pvh.userLanguage(languagetag);
-        pvh.documentPath("/start");
-        startTimeMs = TimeUtils.millis();
-        return ga.postAsync(pvh);
-    }
+	public Future<GoogleAnalyticsResponse> sendStartAppReport() {
+		if (!permission.check())
+			return null;
+		PageViewHit pvh = new PageViewHit("http://gaiasky.com", appname + " start");
+		pvh.applicationName(appname);
+		pvh.applicationVersion(version);
+		pvh.userLanguage(languagetag);
+		pvh.documentPath("/start");
+		startTimeMs = TimeUtils.millis();
+		return ga.postAsync(pvh);
+	}
 
-    public Future<GoogleAnalyticsResponse> sendTimingAppReport() {
-        if (!permission.check())
-            return null;
-        TimingHit th = new TimingHit();
-        th.applicationName(appname);
-        th.applicationVersion(version);
-        th.userLanguage(languagetag);
-        th.pageLoadTime((int) (TimeUtils.millis() - startTimeMs));
-        return ga.postAsync(th);
-    }
+	public Future<GoogleAnalyticsResponse> sendTimingAppReport() {
+		if (!permission.check())
+			return null;
+		TimingHit th = new TimingHit();
+		th.applicationName(appname);
+		th.applicationVersion(version);
+		th.userLanguage(languagetag);
+		th.pageLoadTime((int) (TimeUtils.millis() - startTimeMs));
+		return ga.postAsync(th);
+	}
 
-    public void printResponse(GoogleAnalyticsResponse gar) {
-        System.out.println("Response status code: " + gar.getStatusCode());
-        List<NameValuePair> result = gar.getPostedParms();
-        if (result != null)
-            for (NameValuePair pair : result) {
-                System.out.println(pair.toString());
-            }
-        else
-            System.out.println("No params");
-    }
+	public void printResponse(GoogleAnalyticsResponse gar) {
+		System.out.println("Response status code: " + gar.getStatusCode());
+		List<NameValuePair> result = gar.getPostedParms();
+		if (result != null)
+			for (NameValuePair pair : result) {
+				System.out.println(pair.toString());
+			}
+		else
+			System.out.println("No params");
+	}
 
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // INITIALISE PARAMS
-        Gdx.files = new LwjglFiles();
-        String ASSETS_LOC = "../android/assets/";
-        DateFormatFactory.initialize(new DesktopDateFormatFactory());
-        ConfInit.initialize(new DesktopConfInit(ASSETS_LOC));
-        I18n.initialize(Gdx.files.absolute(ASSETS_LOC + "i18n/gsbundle"));
+		// INITIALISE PARAMS
+		Gdx.files = new LwjglFiles();
+		String ASSETS_LOC = "../android/assets/";
+		DateFormatFactory.initialize(new DesktopDateFormatFactory());
+		ConfInit.initialize(new DesktopConfInit(ASSETS_LOC));
+		I18n.initialize(Gdx.files.absolute(ASSETS_LOC + "i18n/gsbundle"));
 
-        AnalyticsReporting.initialize(new IPermission() {
-            @Override
-            public boolean check() {
-                return true;
-            }
-        });
-        AnalyticsReporting ar = AnalyticsReporting.getInstance();
+		AnalyticsReporting.initialize(new IPermission() {
+			@Override
+			public boolean check() {
+				return true;
+			}
+		});
+		AnalyticsReporting ar = AnalyticsReporting.getInstance();
 
-        // REQUESTS
-        Future<GoogleAnalyticsResponse> fstart = ar.sendStartAppReport();
-        try {
-            GoogleAnalyticsResponse gar = fstart.get(15, TimeUnit.SECONDS);
-            ar.printResponse(gar);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		// REQUESTS
+		Future<GoogleAnalyticsResponse> fstart = ar.sendStartAppReport();
+		try {
+			GoogleAnalyticsResponse gar = fstart.get(15, TimeUnit.SECONDS);
+			ar.printResponse(gar);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        System.out.println();
-        Thread.sleep(2000);
+		System.out.println();
+		Thread.sleep(2000);
 
-        Future<GoogleAnalyticsResponse> ftiming = ar.sendTimingAppReport();
-        try {
-            GoogleAnalyticsResponse gar = ftiming.get(15, TimeUnit.SECONDS);
-            ar.printResponse(gar);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		Future<GoogleAnalyticsResponse> ftiming = ar.sendTimingAppReport();
+		try {
+			GoogleAnalyticsResponse gar = ftiming.get(15, TimeUnit.SECONDS);
+			ar.printResponse(gar);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 }
